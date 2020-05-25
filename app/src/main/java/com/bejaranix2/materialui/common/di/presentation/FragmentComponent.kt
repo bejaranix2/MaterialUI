d1b3e343e43fb822ef4879bd23f24c3d1c02dd61
bejaranix <abejarano@anzen.com.mx>
2020-05-25 18:14:23 -0500

    Button style

--- app/src/main/java/com/bejaranix2/materialui/common/di/presentation/FragmentComponent.kt
+++ app/src/main/java/com/bejaranix2/materialui/common/di/presentation/FragmentComponent.kt
@@ -1,13 +1,7 @@
 package com.bejaranix2.materialui.common.di.presentation
 
-import android.app.Application
-import android.content.Context
-import com.bejaranix2.materialui.common.di.application.ApplicationComponent
-import com.bejaranix2.materialui.screens.activity.ToolbarViewManager
-import com.bejaranix2.materialui.screens.fragment.InitialFragment.InitialFragment
-import com.bejaranix2.materialui.screens.fragment.NextFragment.NextFragment
-import dagger.BindsInstance
-import dagger.Component
+import com.bejaranix2.materialui.screens.fragment.initialfragment.InitialFragment
+import com.bejaranix2.materialui.screens.fragment.nextfragment.NextFragment
 import dagger.Subcomponent
 
 @Subcomponent(modules = [FragmentModule::class])
